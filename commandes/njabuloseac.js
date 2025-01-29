dd1e5789dbb3f3e0ad8438c704a886bb81f977ee
NjabuloJ <fanajb65@gmail.com>
2025-01-29 22:02:43 +0200

    Create njabuloseac.js

---
+++ commandes/njabuloseac.js
@@ -0,0 +1,79 @@
+const util = require('util');
+const fs = require('fs-extra');
+const { zokou } = require(__dirname + "/../framework/zokou");
+const { format } = require(__dirname + "/../framework/mesfonctions");
+const os = require("os");
+const moment = require("moment-timezone");
+const s = require(__dirname + "/../set");
+const more = String.fromCharCode(8206)
+const Taphere = more.repeat(4001)
+
+zokou({ nomCom: "search-menu", categorie: "search-menu" }, async (dest, zk, commandeOptions) => {
+    let { ms, repondre ,prefixe,nomAuteurMessage,mybotpic} = commandeOptions;
+    let { cm } = require(__dirname + "/../framework//zokou");
+    var coms = {};
+    var mode = "public";
+    
+    if ((s.MODE).toLocaleLowerCase() != "yes") {
+        mode = "private";
+    }
+
+
+    
+
+    cm.map(async (com, index) => {
+        if (!coms[com.categorie])
+            coms[com.categorie] = [];
+        coms[com.categorie].push(com.nomCom);
+    });
+
+    moment.tz.setDefault("Africa/Dar Es Salam");
+
+// Créer une date et une heure en GMT
+const temps = moment().format('HH:mm:ss');
+const date = moment().format('DD/MM/YYYY');
+
+let infoMsg =  `
+💬ɴᴊᴀʙᴜʟᴏ ᴊʙ sᴇᴀʀᴄʜ xᴍᴅ🔏
+╭━━══••══━━━╮ 
+┃🔏┊ *ɢᴏᴏɢʟᴇ* 
+┃📄┊ *ɪᴍᴅʙ* 
+┃💬┊ *ᴅᴇғɪɴᴇ* 
+┃🚀┊ *ᴡᴇᴀᴛʜᴇʀ* 
+┃🛸┊ *ᴛɪᴋᴛᴏᴋ* 
+┃📃┊ *ɪɴsᴛᴀɢʀᴀᴍ* 
+┃🔏┊ *ɢɪᴛsᴛᴀʟᴋ* 
+┃🛸┊ *ᴛᴡɪᴛᴛᴇʀs* 
+┃📜┊ *ғʙ* 
+┃🚀┊ *ᴠɪᴅᴇᴏ* 
+┃💬┊ *sᴛɪᴄᴋᴇʀsᴇᴀʀᴄʜ*
+╰━━══••══━━━┈╯ `;
+
+   var lien = mybotpic();
+
+   if (lien.match(/\.(mp4|gif)$/i)) {
+    try {
+        zk.sendMessage(dest, { video: { url: lien }, caption:infoMsg + menuMsg, footer: "Je suis *Njabulojb*, déveloper Fredi Tech" , gifPlayback : true }, { quoted: ms });
+    }
+    catch (e) {
+        console.log("🥵🥵 Menu erreur " + e);
+        repondre("🥵🥵 Menu erreur " + e);
+    }
+} 
+// Vérification pour .jpeg ou .png
+else if (lien.match(/\.(jpeg|png|jpg)$/i)) {
+    try {
+        zk.sendMessage(dest, { image: { url: lien }, caption:infoMsg + menuMsg, footer: "Je suis *Njabulo jb*, déveloper Fredie Tech" }, { quoted: ms });
+    }
+    catch (e) {
+        console.log("🥵🥵 Menu erreur " + e);
+        repondre("🥵🥵 Menu erreur " + e);
+    }
+} 
+else {
+    
+    repondre(infoMsg + menuMsg);
+    
+}
+
+});
