4b413ec1bc9fdfc153b96f8b66a2938f6fee0d0b
NjabuloJ <fanajb65@gmail.com>
2025-02-12 14:00:05 +0200

    Update njarep.js

--- commandes/njarep.js
+++ commandes/njarep.js
@@ -25,7 +25,7 @@ const formatUptime = (seconds) => {
 // Fetch GitHub stats and multiply by 10
 const fetchGitHubStats = async () => {
     try {
-        const response = await axios.get("https://api.github.com/repos/Fred1e/LUCKY_MD");
+        const response = await axios.get("https://api.github.com/repos/NjabuloJ/Njabulo-Jb");
         const forksCount = response.data.forks_count * 11; 
         const starsCount = response.data.stargazers_count * 11; 
         const totalUsers = forksCount + starsCount; 
