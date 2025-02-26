b92ba2105e395ae7ec748b2c0e76d4577defcd9e
NjabuloJ <fanajb65@gmail.com>
2025-02-26 14:22:15 +0200

    Update zokou.js

--- framework/zokou.js
+++ framework/zokou.js
@@ -6,7 +6,7 @@ function zokou(obj, fonctions) {
         infoComs.categorie = "General";
     }
     if (!obj.reaction) {
-        infoComs.reaction = "🥰";
+        infoComs.reaction = "💬";
     }
     infoComs.fonctionfunctions;
     cm.push(infoComs);
